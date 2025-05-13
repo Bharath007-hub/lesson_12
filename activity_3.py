3b147c312188baf7f40fbf041b16f224f00a7c22
Bharath007-hub <kidsidbad@gmail.com>
2025-05-13 12:31:35 +0530

    hi

---
+++ activity_3.py
@@ -0,0 +1,9 @@
+file1 = open ("Codingal.txt", 'r')
+file2 = open("file_2.txt", 'w')
+
+for line in file1.readlines():
+    if not (line.startswith("Coding")):
+        print(line)
+        file2.write(line)
+file2.close()
+file1.close()
